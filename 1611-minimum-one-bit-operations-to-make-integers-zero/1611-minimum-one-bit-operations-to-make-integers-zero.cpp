4fb11d3a3b4c1929b95e86b6131d1dcdddb5fdb3
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-04-22 23:37:11 +0530

    Time: 0 ms (100%), Space: 7.9 MB (97.54%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if (n == 0) return 0;
+
+        int k = 0;
+        while ((1 << (k + 1)) <= n) k++;
+
+        return (1 << (k + 1)) - 1 - minimumOneBitOperations(n ^ (1 << k));
+    }
+};
