89badd531091c76d862d4ec5aa213f90f41ec169
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-05-27 23:33:35 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.97%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int b = sqrt(x);
+        return b;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
