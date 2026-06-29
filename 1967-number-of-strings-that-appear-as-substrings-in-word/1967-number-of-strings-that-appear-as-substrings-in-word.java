d721b034ac218ef28a8fe47beb0087169f4b7359
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-06-29 23:16:56 +0530

    Time: 1 ms (75.75%), Space: 43.4 MB (56.03%) - LeetHub - Time: O(n*m*k), Space: O(1)

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+
+        for (String pattern : patterns) {
+            if (word.contains(pattern)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
