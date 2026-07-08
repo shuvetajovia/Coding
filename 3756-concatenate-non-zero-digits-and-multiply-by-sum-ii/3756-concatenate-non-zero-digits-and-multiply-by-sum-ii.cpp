fbd2a53f239268cf9a45e2c4f125f81c09ccde13
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-07-08 22:52:21 +0530

    Time: 74 ms (31.09%), Space: 172.6 MB (21.24%) - LeetHub - Time: O(n + m * log(m) + q * log(m)), Space: O(n + m)

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    static const int MOD = 1000000007;
+
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.size();
+
+        vector<int> pos;
+        vector<int> digit;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] != '0') {
+                pos.push_back(i);
+                digit.push_back(s[i] - '0');
+            }
+        }
+
+        int m = digit.size();
+
+        vector<long long> pow10(m + 1, 1);
+        for (int i = 1; i <= m; i++)
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+
+        vector<long long> prefHash(m + 1, 0);
+        vector<long long> prefSum(m + 1, 0);
+
+        for (int i = 0; i < m; i++) {
+            prefHash[i + 1] = (prefHash[i] * 10 + digit[i]) % MOD;
+            prefSum[i + 1] = prefSum[i] + digit[i];
+        }
+
+        vector<int> ans;
+
+        for (auto &q : queries) {
+            int l = q[0], r = q[1];
+
+            int L = lower_bound(pos.begin(), pos.end(), l) - pos.begin();
+            int R = upper_bound(pos.begin(), pos.end(), r) - pos.begin() - 1;
+
+            if (L > R) {
+                ans.push_back(0);
+                continue;
+            }
+
+            int len = R - L + 1;
+
+            long long x =
+                (prefHash[R + 1] -
+                 prefHash[L] * pow10[len] % MOD + MOD) % MOD;
+
+            long long sum = prefSum[R + 1] - prefSum[L];
+
+            ans.push_back((x * (sum % MOD)) % MOD);
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
