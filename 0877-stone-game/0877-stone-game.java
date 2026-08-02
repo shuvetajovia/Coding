218da3904dd29534f9ade0a10f488be6c760f6db
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-08-02 23:14:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (35.84%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
