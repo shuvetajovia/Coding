efee1a609e09146063570f2f0a9b0e65e3ac90f5
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-08-14 23:37:04 +0530

    Time: 0 ms (100%), Space: 9 MB (95.5%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+ public:
+  int maximumLengthSubstring(string s) {
+    int ans = 0;
+    vector<int> count(26);
+
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      ++count[s[r] - 'a'];
+      while (count[s[r] - 'a'] > 2)
+        --count[s[l++] - 'a'];
+      ans = max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
