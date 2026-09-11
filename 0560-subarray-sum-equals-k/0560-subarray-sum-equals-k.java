66ecebf34cef8efb8da41676b1ba74660d35dfa8
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-09-11 23:53:21 +0530

    Time: 1543 ms (14.87%), Space: 48.6 MB (70.27%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+
+                sum += nums[j];
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
